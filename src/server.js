7e70f7e2842582c9d7b678d35ab19dcac15993b3
Dennis Blomberg <97sinned@gmail.com>
2026-07-15 15:54:07 +0200

    added bought domain to cors.

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 // Cors
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://pickoneplay.online"],
     credentials: true,
   }),
 );
